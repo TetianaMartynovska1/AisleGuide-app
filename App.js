86c1456459d94d92766e1bdc29ad885e28de03fa
Tetiana Martynovska <tetiana.martynovska1@gmail.com>
2024-07-03 22:45:27 +0300

    add button

--- App.js
+++ App.js
@@ -1,12 +1,16 @@
 import React, { useState } from 'react';
-import { ImageBackground, StyleSheet, Text, TextInput, View, Image, ScrollView, Dimensions } from 'react-native';
+import { ImageBackground, StyleSheet, Text, TextInput, View, Image, ScrollView, Dimensions, TouchableOpacity } from 'react-native';
 
 const image = { uri: 'https://img.freepik.com/premium-photo/white-abstract-background_882595-3799.jpg' };
-const containerImage = { uri: 'https://cdn.britannica.com/22/187222-050-07B17FB6/apples-on-a-tree-branch.jpg' }; // Replace with your image URL
-const { width } = Dimensions.get('window');
+const containerImage = { uri: 'https://cdn.britannica.com/22/187222-050-07B17FB6/apples-on-a-tree-branch.jpg' };
+const { width, height } = Dimensions.get('window');
 
 const App = () => {
   const [search, setSearch] = useState('');
+
+  const handlePress = () => {
+    alert('Button pressed');
+  };
 
   return (
     <View style={styles.container}>
@@ -26,12 +30,15 @@ const App = () => {
               <View key={index} style={styles.card}>
                 <Image source={containerImage} style={styles.cardImage} />
                 <Text style={styles.cardText}>Red Apple</Text>
-                
+                <View style={styles.buttonContainer}>
+                  <TouchableOpacity style={styles.button} onPress={handlePress}>
+                    <Text style={styles.buttonText}>Click Me</Text>
+                  </TouchableOpacity>
+                </View>
               </View>
-              
             ))}
           </ScrollView>
-          
+          {/* <View style={styles.bottomSpace} /> */}
         </View>
       </ImageBackground>
     </View>
@@ -41,6 +48,7 @@ const App = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    height: height * 0.9, // Set container height to 90% of the screen height
   },
   image: {
     flex: 1,
@@ -54,15 +62,16 @@ const styles = StyleSheet.create({
     width: '100%',
   },
   searchContainer: {
-    width: '80%',
+    width: width * 0.8,
     backgroundColor: '#FFFFFF',
-     borderWidth: 1,
+    borderWidth: 1,
     borderColor: '#000000',
     borderRadius: 5,
     paddingHorizontal: 10,
     paddingVertical: 5,
-    marginTop: '20%',
+    marginTop: height * 0.1,
     zIndex: 1,
+    marginBottom: 10,
   },
   searchInput: {
     height: 40,
@@ -73,6 +82,7 @@ const styles = StyleSheet.create({
     flexWrap: 'wrap',
     justifyContent: 'space-around',
     paddingVertical: 20,
+    marginBottom: 10,
   },
   card: {
     width: width * 0.4, // Adjusting width to fit 2 cards per row
@@ -97,6 +107,27 @@ const styles = StyleSheet.create({
     fontSize: 16,
     textAlign: 'center',
     marginTop: 10,
+  },
+  buttonContainer: {
+    width: '100%', // Adjusted to take full width of the card
+    alignItems: 'center',
+  },
+  button: {
+    backgroundColor: '#F83758',
+    width: '80%', // 80% of the button container width
+    borderRadius: 5,
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: height*0.04,
+    marginBottom: height*0.03,
+  },
+  buttonText: {
+    fontSize: 12,
+    color: '#FFFFFF',
+    textAlign: 'center',
+  },
+  bottomSpace: {
+    height: 20, // Adjust as needed for the space below the cards
   },
 });
 
